8b94dcfca7b2f41bc6d770f1c8aef2c0696c007a
Haji Abubaker Sidiq <hajiabubakersidiq2002@gmail.com>
2025-05-25 20:14:09 +0530

    third commit

--- src/assets/Skills.js
+++ src/assets/Skills.js
@@ -1,15 +1,15 @@
 import Html from "../assets/html5.png";
-import Css from "../assets/css.png";
-import Bootstrap from "../assets/bootst.png";
-import Javascript from "../assets/javascript.png";
-import React from "../assets/reactjs.png";
-import Python from "../assets/python.png";
-import Mysql from "../assets/mysql.png";
-import Django from "../assets/django.png";
-import Git from "../assets/Git-1.png";
-import Vscode from "../assets/vs.png";
-import Json from "../assets/json.png";
-import Github from "../assets/github.png";
+import Css from "./css.png";
+import Bootstrap from "./bootst.png";
+import Javascript from "./javascript.png";
+import React from "./reactjs.png";
+import Python from "./python.png";
+import Mysql from "./mysql.png";
+import Django from "./django.png";
+import Git from "./Git-1.png";
+import Vscode from "./vs.png";
+import Json from "./json.png";
+import Github from "./github.png";
 
 export const skills = [
   { id: 1, icon: Html },
@@ -29,14 +29,14 @@ export const tools = [
   { id: 3, icon: Vscode },
 ];
 
-import Picture from "../assets/Picture.png";
-import Food1 from "../assets/Food1.jpg";
-import Food2 from "../assets/Food2.png";
-import Travel1 from "../assets/travel-1.jpg";
-import Travel2 from "../assets/Travel2.jpg";
-import Movie from "../assets/Movie.jpg";
-import WeatherImg from "../assets/weather.jpg";
-import Todo from "../assets/Todo.jpg";
+import Picture from "./Picture.png";
+import Food1 from "./Food1.jpg";
+import Food2 from "./Food2.png";
+import Travel1 from "./travel-1.jpg";
+import Travel2 from "./Travel2.jpg";
+import Movie from "./Movie.jpg";
+import weatherImage from "./weather.jpg";
+import Todo from "./Todo.jpg";
 
 export const Frontend = [
   {
@@ -53,7 +53,7 @@ export const Frontend = [
     projectName: "Food Order Web App",
     description:
       "A React-based online platform for customers to browse and order food with features like cart management and order tracking.",
-    technologies:  "HTML / CSS, JavaScript, React",
+    technologies: "HTML / CSS, JavaScript, React",
     githubLink: "https://github.com/Sidiq2002/Food-Order-App",
     liveLink: "https://food-order-app-ten-lake.vercel.app/",
   },
@@ -63,7 +63,7 @@ export const Frontend = [
     projectName: "Travel Blog Web App",
     description:
       "A dynamic React platform showcasing travel experiences, destinations, and stories through engaging blog posts and stunning visuals.",
-    technologies:  "HTML / CSS, JavaScript, React",
+    technologies: "HTML / CSS, JavaScript, React",
     githubLink: "https://github.com/Sidiq2002/Travel-Blog",
     liveLink: "https://travel-blog-reactjs.netlify.app/",
   },
@@ -73,16 +73,16 @@ export const Frontend = [
     projectName: "MovieHUB Web App",
     description:
       "A React-based movie library platform for browsing, searching, and discovering movies with ratings, reviews, and recommendations.",
-    technologies:  "HTML / CSS, JavaScript, React",
+    technologies: "HTML / CSS, JavaScript, React",
     githubLink: "https://github.com/Sidiq2002/MovieHUB",
     liveLink: "https://movie-hub-iota-jade.vercel.app/login",
   },
   {
-    image: WeatherImg,
+    image: weatherImage,
     projectName: "Weather App",
     description:
       "A JavaScript-based app providing real-time weather updates and forecasts for locations worldwide.",
-    technologies:  "HTML / CSS, JavaScript",
+    technologies: "HTML / CSS, JavaScript",
     githubLink: "https://github.com/Sidiq2002/Weather-App",
     liveLink: "https://weather-app-javascript-live.netlify.app/",
   },
@@ -91,7 +91,7 @@ export const Frontend = [
     projectName: "To-Do App",
     description:
       "A JavaScript-based task management tool for creating, editing, and managing to-do lists.",
-    technologies:  "HTML / CSS, JavaScript",
+    technologies: "HTML / CSS, JavaScript",
     githubLink: "https://github.com/Sidiq2002/To-Do-App",
     liveLink: "https://to-do-application-task-management.netlify.app/",
   },
